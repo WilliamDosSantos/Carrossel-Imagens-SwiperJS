0a19f8e8ffc4be5c914a8ec54d397cb503361823
William Lucas <williamsantos1506@gmail.com>
2022-07-07 21:12:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  keyboard: true,
+});
